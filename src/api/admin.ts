16acd08f72d27c792e37063ccbb1c17e10dc15d6
Your Name <kimthanhdung230601@gmail.com>
2024-05-16 14:54:26 +0700

    feature: test form exam 1.1

--- src/api/admin.ts
+++
@@ -1,5 +0,0 @@
-import { sendDelete, sendGet, sendPost, sendPut } from './api';
-
-export const getListData = () => sendGet(`/example`);
-export const updateData = (payload: any, param: string) =>
-  sendPost(`/example/${param}`);
